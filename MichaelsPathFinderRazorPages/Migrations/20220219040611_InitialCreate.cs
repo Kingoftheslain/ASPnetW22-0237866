d4ef9762fb002c547a60a656fa917b589ae98841
Michael MacNeill <98714598+Kingoftheslain@users.noreply.github.com>
2022-02-18 23:15:28 -0500

    Finished Modeling

---
+++ MichaelsPathFinderRazorPages/Migrations/20220219040611_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MichaelsPathFinderRazorPages.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feats",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    featName = table.Column<string>(type: "TEXT", nullable: true),
+                    FeatLearnedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    shortDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feats", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feats");
+        }
+    }
+}
